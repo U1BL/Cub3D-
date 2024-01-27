821e1836756fc637e7d3d96c47bc2151dde08f4f
Mohammad Faisal <mohammedalshwayyat@icloud.com>
2024-01-27 18:02:44 +0400

    Initial commit

---
+++ get_next_linee/get_next_line.h
@@ -0,0 +1,20 @@
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 10
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+char	*read_buffer_size(int fd, char *str);
+char	*ft_line(char *str);
+char	*ft_remain(char *str);
+
+char	*get_next_line(int fd);
+
+char	*ft_gnl_strjoin(char *s1, char *s2);
+char	*ft_strchr_get(const char *s, int c);
+size_t	ft_strlen(const char *s);
+
+#endif
---
+++ parsing.c
@@ -0,0 +1,84 @@
+
+#include "cub3D.h"
+
+void	ft_map_valid_structure(t_all *all, int *i, size_t *nl)
+{
+	while (all->mapl[++(*i)] && *nl < 7)
+	{
+		if (all->mapl[*i] == '\n')
+			*i = *i + 1;
+		else if (all->mapl[*i] != '\n')
+		{
+			*nl = *nl + 1;
+			while (all->mapl[*i] != '\n')
+				*i = *i + 1;
+		}
+	}
+}
+
+void	ft_map_valid_char(t_all *all, int i, size_t nl)
+{
+	ft_map_valid_structure(all, &i, &nl);
+	while (all->mapl[i])
+	{
+		if (all->mapl[i] == '1' || all->mapl[i] == '0' || all->mapl[i] == 'N'
+			|| all->mapl[i] == 'S' || all->mapl[i] == 'E' || all->mapl[i] == 'W'
+			|| all->mapl[i] == ' ' || all->mapl[i] == '\n')
+		{
+			check_pos(all->mapl[i], all);
+			if (all->mapl[i] == '\n'
+				&& all->mapl[i + 1] == '\n' && all->mapl[i + 2])
+				ft_error(all, 8);
+		}
+		else
+			ft_error(all, 7);
+		i++;
+	}
+}
+
+char	**ft_split_all(char *str, t_all *all)
+{
+	char	**ret;
+	size_t	wordc;
+	size_t	i;
+	size_t	x;
+
+	wordc = ft_wordcount(str);
+	ret = (char **)malloc((wordc + 1) * sizeof(char *));
+	if (!ret)
+		ft_error(all, 4);
+	i = 0;
+	while (i < wordc)
+	{
+		while (*str && *str == '\n')
+			str++;
+		x = 0;
+		while (str[x] && str[x] != '\n')
+			x++;
+		ret[i] = (char *)malloc(x + 1);
+		x = 0;
+		while (*str && *str != '\n')
+			ret[i][x++] = *str++;
+		ret[i++][x] = 0;
+	}
+	ret[i] = NULL;
+	return (ret);
+}
+
+int	ft_find_index(t_all *all, char **arr, char *s, size_t size)
+{
+	int	i;
+
+	i = 0;
+	ft_arr_trim(arr);
+	if (!arr)
+		ft_error(all, 5);
+	while (arr[i] && i < 6)
+	{
+		if (!ft_strncmp(arr[i], s, size))
+			return (i);
+		i++;
+	}
+	ft_error(all, 5);
+	return (i);
+}
